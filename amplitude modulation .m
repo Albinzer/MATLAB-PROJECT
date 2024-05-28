4925a4b26c73e252c1a83e4cb5b81614ff0a1de2
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-05-29 00:20:39 +0600

    Update Amplitude modulation .m

--- amplitude modulation .m
+++ amplitude modulation .m
@@ -1,9 +1,38 @@
 clc;
 clear all;
-close all;
+m_index = input ('enter the value of mod index: ');
+t=0:0.001:3;
 
-fm=10;
-t=0:0.0001:1;
-fc=15;
-ms=40;
+Am =1;
+Ac = Am / m_index;
 
+
+fm= 1;
+fc=25;
+
+mt = cos (2*pi*fm*t );
+ct =cos (2*pi*fc*t);
+
+am= (1+m_index*mt) .* (Ac*ct);
+
+subplot(3,1,1)
+plot(t,mt); 
+xlabel =('Time');
+ylabel = ('Message Signal');
+title ('Modulating Signal')
+grid on 
+
+
+subplot(3,1,2);
+plot(t,ct);
+xlabel =('Time');
+ylabel = ('Carrier Signal');
+title (' Carrier Signal');
+grid on 
+
+subplot(3,1,3);
+plot(t,am);
+xlabel =('Time');
+ylabel = ('Amplitude Signal');
+title (' Amplitude  Signal');
+grid on 
