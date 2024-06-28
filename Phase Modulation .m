9cf0fafba28aa0d3116fe81290fb9f8604ddf352
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-06-29 03:28:55 +0600

    Update Phase Modulation .m

--- Phase Modulation .m
+++ Phase Modulation .m
@@ -2,13 +2,40 @@ clc;
 clear all;
 close all;
 
-fm=10;
-fc=15;
-ms=35;
-t=0:0.0001:1;
+fm = 10; % Message signal frequency
+fc = 15; % Carrier signal frequency
+ms = 35; % Modulation index 
+t = 0:0.0001:1; % Time vector
 
-title("Phase Modulation");
+% Message signal (modulating signal)
+mt = cos(2 * pi * fm * t);
+
+% Carrier signal
+ct = cos(2 * pi * fc * t);
+
+% Phase modulated signal
+pt = cos(2 * pi * fc * t + ms * sin(2 * pi * fm * t));
+
+% Plot the message signal
 subplot(3,1,1);
-plot("t,f");
-xlabel('Amplitude');
-ylabel('Time');
+plot(t, mt);
+title('Message Signal');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
+
+% Plot the carrier signal
+subplot(3,1,2);
+plot(t, ct);
+title('Carrier Signal');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
+
+% Plot the phase modulated signal
+subplot(3,1,3);
+plot(t, pt);
+title('Phase Modulation Signal');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
